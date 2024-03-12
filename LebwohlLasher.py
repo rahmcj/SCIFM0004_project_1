4faea4eee63da303a782ad79fc644b3be9c26999
rm13819@bristol.ac.uk <rm13819@bristol.ac.uk>
2024-03-12 14:44:39 +0000

    Attempting to see if np.cos(ang) * np.cos(ang is faster than np.cos(ang)**2

--- LebwohlLasher.py
+++ LebwohlLasher.py
@@ -152,14 +152,15 @@ def one_energy(arr,ix,iy,nmax):
 # Add together the 4 neighbour contributions
 # to the energy
 #
+    
     ang = arr[ix,iy]-arr[ixp,iy]
-    en += 0.5*(1.0 - 3.0*np.cos(ang)**2)
+    en += 0.5*(1.0 - 3.0*np.cos(ang)*np.cos(ang))
     ang = arr[ix,iy]-arr[ixm,iy]
-    en += 0.5*(1.0 - 3.0*np.cos(ang)**2)
+    en += 0.5*(1.0 - 3.0*np.cos(ang)*np.cos(ang))
     ang = arr[ix,iy]-arr[ix,iyp]
-    en += 0.5*(1.0 - 3.0*np.cos(ang)**2)
+    en += 0.5*(1.0 - 3.0*np.cos(ang)*np.cos(ang))
     ang = arr[ix,iy]-arr[ix,iym]
-    en += 0.5*(1.0 - 3.0*np.cos(ang)**2)
+    en += 0.5*(1.0 - 3.0*np.cos(ang)*np.cos(ang))
     return en
 #=======================================================================
 def all_energy(arr,nmax):
